a0874495a15e39cfbea13c62aec007270595b3ba
MuratYucedag <yucedagmurat23@gmail.com>
2026-02-11 17:25:12 +0300

    Add project files.

---
+++ DatabaseMastery.TransportMongoDb/Mapping/GeneralMapping.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using DatabaseMastery.TransportMongoDb.Dtos.AboutDtos;
+using DatabaseMastery.TransportMongoDb.Dtos.BrandDtos;
+using DatabaseMastery.TransportMongoDb.Dtos.GetInTouchDtos;
+using DatabaseMastery.TransportMongoDb.Dtos.HowItWorkDtos;
+using DatabaseMastery.TransportMongoDb.Dtos.OfferDtos;
+using DatabaseMastery.TransportMongoDb.Dtos.ProjectSectionDtos;
+using DatabaseMastery.TransportMongoDb.Dtos.SliderDtos;
+using DatabaseMastery.TransportMongoDb.Dtos.TestimonialDtos;
+using DatabaseMastery.TransportMongoDb.Entities;
+
+namespace DatabaseMastery.TransportMongoDb.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Slider, ResultSliderDto>().ReverseMap();
+            CreateMap<Slider, CreateSliderDto>().ReverseMap();
+            CreateMap<Slider, UpdateSliderDto>().ReverseMap();
+            CreateMap<Slider, GetSliderByIdDto>().ReverseMap();
+
+            CreateMap<Brand, ResultBrandDto>().ReverseMap();
+            CreateMap<Brand, CreateBrandDto>().ReverseMap();
+            CreateMap<Brand, UpdateBrandDto>().ReverseMap();
+            CreateMap<Brand, GetBrandByIdDto>().ReverseMap();
+
+            CreateMap<Offer, ResultOfferDto>().ReverseMap();
+            CreateMap<Offer, CreateOfferDto>().ReverseMap();
+            CreateMap<Offer, UpdateOfferDto>().ReverseMap();
+            CreateMap<Offer, GetOfferByIdDto>().ReverseMap();
+
+            CreateMap<About, ResultAboutDto>().ReverseMap();
+            CreateMap<About, CreateAboutDto>().ReverseMap();
+            CreateMap<About, UpdateAboutDto>().ReverseMap();
+            CreateMap<About, GetAboutByIdDto>().ReverseMap();
+
+            CreateMap<GetInTouch, ResultGetInTouchDto>().ReverseMap();
+            CreateMap<GetInTouch, UpdateGetInTouchDto>().ReverseMap();
+            CreateMap<GetInTouch, CreateGetInTouchDto>().ReverseMap();
+            CreateMap<GetInTouch, GetGetInTouchByIdDto>().ReverseMap();
+
+            CreateMap<HowItWork, ResultHowItWorkDto>().ReverseMap();
+            CreateMap<HowItWork, CreateHowItWorkDto>().ReverseMap();
+            CreateMap<HowItWork, UpdateHowItWorkDto>().ReverseMap();
+            CreateMap<HowItWork, GetHowItWorkByIdDto>().ReverseMap();
+
+            CreateMap<Testimonial, ResultTestimonialDto>().ReverseMap();
+            CreateMap<Testimonial, CreateTestimonialDto>().ReverseMap();
+            CreateMap<Testimonial, UpdateTestimonialDto>().ReverseMap();
+            CreateMap<Testimonial, GetTestimonialByIdDto>().ReverseMap();
+
+            CreateMap<ProjectSection, ResultProjectSectionDto>().ReverseMap();
+            CreateMap<ProjectSection, UpdateProjectSectionDto>().ReverseMap();
+            CreateMap<ProjectSection, CreateProjectSectionDto>().ReverseMap();
+            CreateMap<ProjectSection, GetProjectSectionByIdDto>().ReverseMap();
+        }
+    }
+}
